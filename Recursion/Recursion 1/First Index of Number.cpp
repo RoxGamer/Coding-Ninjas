8a63aea9a87e5ea2cc19aef1ea4f9c173cdc396e
Rox <67151410+RoxGamer@users.noreply.github.com>
2022-11-10 22:32:59 +0530

    Create First Index of Number.cpp

---
+++ Recursion/Recursion 1/First Index of Number.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+int firstIndex(int input[], int size, int x) {
+	if(size == 0){
+        return -1;
+    }
+    if(x==*input){
+        return 0;
+    }
+    int ans = firstIndex(input+1,size-1,x);
+    if(ans == -1){
+        return -1;
+    }
+    else{
+        return ans+1;
+    }
+}
+
+int main(){
+    int n;
+    cin >> n;
+  
+    int *input = new int[n];
+    
+    for(int i = 0; i < n; i++) {
+        cin >> input[i];
+    }
+    
+    int x;
+    
+    cin >> x;
+    
+    cout << firstIndex(input, n, x) << endl;
+
+}
